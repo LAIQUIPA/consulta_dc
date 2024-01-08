103598ac3ebaeb1a8fda9fb04e88e99fe663aa71
LAIQUIPA <LAIQUIPA@autonoma.edu.pe>
2024-01-07 19:09:05 -0500

    web

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -3,6 +3,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:itse_validador/api/gsheets..dart';
 //import 'package:itse_validador/components/certificado_dialog.dart';
 import 'package:rive/rive.dart';
+//test
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
